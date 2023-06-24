be31f3f5e9fdf762f204b01ff7710ff428ba9fc7
Abhi <abhimalakar360@gmail.com>
2023-06-24 12:05:49 +0530

    Loader sucessfully working

---
+++ src/app/load/load.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+    selector: 'app-load',
+    templateUrl: './load.component.html',
+    styleUrls: ['./load.component.css']
+})
+export class LoadComponent implements OnInit {
+    dt: any;
+    dataDisplay: any;
+    constructor(private http: HttpClient) {
+    }
+  
+    ngOnInit(): void {
+        this.http.get(
+'http://www.mocky.io/v2/5ec6a61b3200005e00d75058')
+            .subscribe(Response => {
+                  
+                // If Response comes function
+                // hideloader() is called
+                if (Response) {
+                    hideloader();
+                }
+                console.log(Response)
+                this.dt = Response;
+                this.dataDisplay = this.dt.data;
+            });
+        // Function is defined
+        function hideloader() {
+  
+            // Setting display of spinner
+            // element to none
+            // document.getElementById('loading').style.display = 'none';
+        }
+    }
+}
